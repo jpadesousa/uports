a35a847960df25fb69a4d9f105155de1263efdcf
João Pedro Agostinho de Sousa <joao.agostinho-de-sousa@psi.ch>
2026-07-20 15:22:32 +0200

    update before release v0.1.0

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"slices"
 	"sort"
 	"strconv"
+	"strings"
 
 	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/fang"
@@ -21,25 +22,20 @@ import (
 	"github.com/spf13/pflag"
 )
 
-// Color style variables
+// Color and column style variables
 var (
-	usernameStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("45")).
-			Bold(true)
+	usernameStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("45")).Bold(true)
+	uidStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("11")).Bold(true)
+	columnTitleStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+	helpTitleStyle   = lipgloss.NewStyle().Foreground(charmtone.Charple).Bold(true)
+	portStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Bold(true)
+	pidStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Bold(true)
+	cmdStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Bold(true)
 
-	uidStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("11")).
-			Bold(true)
-
-	columnTitleStyle = lipgloss.NewStyle().
-				Foreground(lipgloss.Color("244"))
-)
-
-// Message colors
-var (
-	titleStyle = lipgloss.NewStyle().
-		Foreground(charmtone.Charple).
-		Bold(true)
+	portCol     = lipgloss.NewStyle().Width(5)
+	pidCol      = lipgloss.NewStyle().Width(6)
+	nSocketsCol = lipgloss.NewStyle().Width(3)
+	cmdCol      = lipgloss.NewStyle().MaxWidth(61)
 )
 
 // Flag variables
@@ -66,7 +62,7 @@ func main() {
 
 Displays listening TCP ports grouped by user,
 along with the owning process ID and command.`,
-			titleStyle.Render("DESCRIPTION")),
+			helpTitleStyle.Render("DESCRIPTION")),
 		RunE: runApp,
 	}
 
@@ -75,42 +71,26 @@ along with the owning process ID and command.`,
 	// =============================================
 
 	// search
-	searchFS.Uint32VarP(
-		&cfg.search.port,
-		"port",
-		"p",
-		0,
+	searchFS.Uint32VarP(&cfg.search.port, "port", "p", 0,
 		"Filter by listening TCP port",
 	)
 
-	searchFS.Int32VarP(
-		&cfg.search.pid,
-		"pid",
-		"i",
-		0,
+	searchFS.Int32VarP(&cfg.search.pid, "pid", "i", 0,
 		"Filter by process ID",
 	)
 
-	searchFS.StringVarP(
-		&cfg.search.cmd,
-		"cmd",
-		"c",
-		"",
+	searchFS.StringVarP(&cfg.search.cmd, "cmd", "c", "",
 		"Filter by command (substring match)",
 	)
 
-	searchFS.StringSliceVarP(
-		&cfg.search.user,
-		"user",
-		"u",
-		nil,
+	searchFS.StringSliceVarP(&cfg.search.user, "user", "u", nil,
 		"Filter by username or UID (comma-separated values)",
 	)
 
 	cobraCmd.Flags().AddFlagSet(searchFS)
 
 	// =============================================
-	// Execute command
+	// Execute app
 	// =============================================
 
 	// Set new flag display settings
@@ -134,43 +114,42 @@ along with the owning process ID and command.`,
 func runApp(cobraCmd *cobra.Command, args []string) error {
 
 	type userConnection struct {
-		port uint32
-		pid  int32
-		cmd  string
+		port      uint32
+		pid       int32
+		cmd       string
+		nSockets  uint32
+		colorPort bool
+		colorPid  bool
+		colorCmd  bool
 	}
+	userConn := make(map[string][]userConnection)
 
+	// pidCache serves to avoid having duplicated information for the same PID
 	type procInfo struct {
 		username string
 		cmd      string
 	}
+	procCache := map[int32]procInfo{}
 
-	userConn := make(map[string][]userConnection)
-
-	// procCache serves to avoid having duplicated information for the same PID
-	procCache := make(map[int32]procInfo)
-
-	// Get all listening TCP sockets
-	conns, err := net.Connections("tcp")
+	// Get all network connections
+	// Chosen ConnectionsPid instead of Connections to have a small performance
+	// benefit when the user searches by PID
+	TCPconns, err := net.ConnectionsPid("tcp", cfg.search.pid)
 	if err != nil {
 		panic(err)
 	}
 
 	// ------------------------------------------------ //
-	// For each socket
+	// Gather info for each sockets
 	// ------------------------------------------------ //
-	for _, c := range conns {
+	for _, c := range TCPconns {
 
-		// Only LISTEN ports
+		// Define the TCP status to display
 		if c.Status != "LISTEN" {
 			continue
 		}
 
-		// Exclude PID 0
-		if c.Pid == 0 {
-			continue
-		}
-
-		_, ok := procCache[c.Pid]
+		info, ok := procCache[c.Pid]
 
 		if !ok {
 
@@ -180,19 +159,8 @@ func runApp(cobraCmd *cobra.Command, args []string) error {
 				continue
 			}
 
-			// Gather UIDs associated with PID
-			uids, err := p.Uids()
-			if err != nil || len(uids) == 0 {
-				continue
-			}
-
-			// Skip root
-			if uids[0] == 0 {
-				continue
-			}
-
 			// Lookup user and convert UID to string
-			u, err := user.LookupId(strconv.FormatUint(uint64(uids[0]), 10))
+			u, err := user.LookupId(strconv.FormatUint(uint64(c.Uids[0]), 10))
 			if err != nil {
 				continue
 			}
@@ -204,32 +172,175 @@ func runApp(cobraCmd *cobra.Command, args []string) error {
 			}
 
 			// Add to process cache
-			procCache[c.Pid] = procInfo{
+			info = procInfo{
 				username: u.Username,
 				cmd:      cmd,
 			}
+			procCache[c.Pid] = info
+		}
 
-			userConn[u.Username] =
-				append(userConn[u.Username],
-					userConnection{port: c.Laddr.Port, pid: c.Pid, cmd: cmd})
+		// Check if we've already added this PID and PORT for this user.
+		found := false
+		for i := range userConn[info.username] {
+			if userConn[info.username][i].pid == c.Pid &&
+				userConn[info.username][i].port == c.Laddr.Port {
+
+				userConn[info.username][i].nSockets++
+				found = true
+				break
+
+			}
+		}
+
+		if !found {
+			userConn[info.username] =
+				append(userConn[info.username], userConnection{
+					port:     c.Laddr.Port,
+					pid:      c.Pid,
+					cmd:      info.cmd,
+					nSockets: 1,
+				})
+		}
+	}
+
+	// Collect all usernames
+	allUsers := slices.Collect(maps.Keys(userConn))
+
+	// Create an empty slice to fill filtered usernames
+	var filteredUsers []string
+
+	// The filters below will modify the contents of userConn
+	// (map[string][]userConnection) so that if it will only keep the
+	// userConnection structs that pass the filter
+
+	// ------------------------------------------------ //
+	// Handle ports
+	// ------------------------------------------------ //
+
+	if cobraCmd.Flags().Changed("port") {
+
+		for _, username := range allUsers {
+			conns := userConn[username]
+
+			filtered := conns[:0]
+
+			for i := range conns {
+				if conns[i].port == cfg.search.port {
+					conns[i].colorPort = true
+					filtered = append(filtered, conns[i])
+				}
+			}
+
+			userConn[username] = filtered
+
+			if len(filtered) > 0 {
+				filteredUsers = append(filteredUsers, username)
+			}
 		}
 	}
 
 	// ------------------------------------------------ //
-	// Print users alphabetically and with sorted ports
+	// Handle pids
 	// ------------------------------------------------ //
+	if cobraCmd.Flags().Changed("pid") {
 
-	var users []string
-	if cfg.search.user != nil {
-		users = cfg.search.user
-		sort.Strings(users)
-	} else {
-		users = slices.Collect(maps.Keys(userConn))
-		sort.Strings(users)
+		if filteredUsers == nil {
+			filteredUsers = allUsers
+		}
+
+		for _, username := range filteredUsers {
+			conns := userConn[username]
+
+			filtered := conns[:0]
+
+			for i := range conns {
+				if conns[i].pid == cfg.search.pid {
+					conns[i].colorPid = true
+					filtered = append(filtered, conns[i])
+				}
+			}
+
+			userConn[username] = filtered
+
+			if len(filtered) > 0 {
+				filteredUsers = append(filteredUsers, username)
+			}
+		}
 	}
 
-	for _, username := range users {
+	// ------------------------------------------------ //
+	// Handle cmd
+	// ------------------------------------------------ //
+	if cfg.search.cmd != "" {
+
+		if filteredUsers == nil {
+			filteredUsers = allUsers
+		}
+
+		for _, username := range filteredUsers {
+			conns := userConn[username]
+
+			filtered := conns[:0]
+
+			for i := range conns {
+				if strings.Contains(conns[i].cmd, cfg.search.cmd) {
+					conns[i].colorCmd = true
+					filtered = append(filtered, conns[i])
+				}
+			}
+
+			userConn[username] = filtered
+
+			if len(filtered) > 0 {
+				filteredUsers = append(filteredUsers, username)
+			}
+		}
+	}
+
+	// ------------------------------------------------ //
+	// Handle users
+	// ------------------------------------------------ //
+
+	// Process user flag or collect all users
+	if cfg.search.user != nil {
+
+		// users in the --users flag takes priority
+		filteredUsers = nil
+
+		for _, s := range cfg.search.user {
+			username, err := lookupUsername(s)
+			if err != nil {
+				continue
+			}
+			filteredUsers = append(filteredUsers, username)
+		}
+	} else {
+		// If no users where filtered just include all users
+		if filteredUsers == nil {
+			filteredUsers = allUsers
+		}
+		sort.Strings(filteredUsers)
+	}
+
+	// Keep only unique usernames
+	var UniqueUsers []string
+	seen := make(map[string]struct{})
+	for _, user := range filteredUsers {
+		if _, ok := seen[user]; !ok {
+			seen[user] = struct{}{}
+			UniqueUsers = append(UniqueUsers, user)
+		}
+	}
+
+	userCount := 0
+	for _, username := range UniqueUsers {
+
 		conns := userConn[username]
+		// If there are no entries in the user's connection because they were
+		// filtered out, move to the next user
+		if len(conns) == 0 {
+			continue
+		}
 
 		// Sort this user's connections by port
 		slices.SortFunc(conns, func(a, b userConnection) int {
@@ -241,33 +352,75 @@ func runApp(cobraCmd *cobra.Command, args []string) error {
 			continue
 		}
 
+		// Leave a gap between user tables
+		if userCount > 0 {
+			fmt.Println()
+		}
+
+		// Render username and uid
 		fmt.Println(
 			usernameStyle.Render(
-				fmt.Sprintf("\n%s", username)),
+				fmt.Sprintf("%s", username)),
 			uidStyle.Render(
 				fmt.Sprintf("(%s)", u.Uid),
 			))
 
+		// Print table values for each user
 		fmt.Println(columnTitleStyle.Render(
-			fmt.Sprintf("  %-5s %-6s %s", "PORT", "PID", "COMMAND"),
+			fmt.Sprintf("  %-5s %-6s %-3s %s", "PORT", "PID", "N", "COMMAND"),
 		))
+
 		for _, c := range conns {
-			fmt.Printf("  %-5d %-6d %s\n",
-				c.port,
-				c.pid,
-				truncateChars(c.cmd, 60),
+
+			port := strconv.FormatUint(uint64(c.port), 10)
+			pid := strconv.FormatUint(uint64(c.pid), 10)
+			nSockets := strconv.FormatUint(uint64(c.nSockets), 10)
+
+			if c.colorPort {
+				port = portStyle.Render(port)
+			}
+
+			if c.colorPid {
+				pid = pidStyle.Render(pid)
+			}
+
+			if c.colorCmd {
+				c.cmd = cmdStyle.Render(c.cmd)
+			} else {
+				c.cmd = cmdCol.Render(c.cmd)
+			}
+
+			fmt.Println(
+				"  " +
+					portCol.Render(port) + " " +
+					pidCol.Render(pid) + " " +
+					nSocketsCol.Render(nSockets) + " " +
+					c.cmd,
 			)
 		}
+
+		userCount++
 	}
 
 	return nil
 }
 
-// truncateChars truncates a string to a maximum number of characters.
-func truncateChars(s string, maxChars int) string {
-	runes := []rune(s)
-	if len(runes) <= maxChars {
-		return s
+// lookupUsername converts a string into a integer and search the user based on
+// UID. If that fails, it will search the user based on username.
+func lookupUsername(s string) (string, error) {
+	var u *user.User
+	var err error
+
+	if _, err = strconv.Atoi(s); err == nil {
+		// It's a UID
+		u, err = user.LookupId(s)
+	} else {
+		// It's a username
+		u, err = user.Lookup(s)
 	}
-	return string(runes[:maxChars-3]) + "..."
+	if err != nil {
+		return "", err
+	}
+
+	return u.Username, nil
 }
